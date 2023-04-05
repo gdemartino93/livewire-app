7892c5aaf0b94990fd57dbb8c85797ae35ba7789
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-05 23:41:27 +0200

    error snip

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -4,9 +4,9 @@
             <div class="mb-3">
                 <label for="title" class="form-label">Inserisci il titolo(max 10 caratteri)</label>
                 {{-- mostra l'errore sulle validazioni --}}
-                @if ($errors -> has('title'))    
-                <div class="alert alert-danger">{{ $errors->first('title') }}</div>
-                @endif
+                @error('title')
+                    <div class="alert alert-danger">{{ $errors->first('title') }}</div>
+                @enderror
                 <input wire:model='title' type="text" class="form-control" placeholder="Inserisci il titolo">
                 <span class={{ $this->titleLength > 10 ? 'text-danger' : 'text-success' }}>{{ $this->titleLength }}/10</span>
                 @if ($this->titleLength > 10)
@@ -16,9 +16,9 @@
               <div class="mb-3">
                 <label for="body" class="form-label">Inserisci il testo</label>
                 {{-- mostra l'errore sulle validazioni --}}
-                @if ($errors -> has('body'))    
-                <div class="alert alert-danger">{{ $errors->first('body') }}</div>
-                @endif
+                @error('body')     
+                    <div class="alert alert-danger">{{ $errors->first('body') }}</div>
+                @enderror
                 <textarea wire:model='body' class="form-control" rows="3"></textarea>
             </div>
             <select wire:model='color' class="form-select form-select-sm" aria-label=".form-select-sm example">
